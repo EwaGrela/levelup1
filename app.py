0ffbdfa2540b92e77a8b5054f4c2a1188a5e42d7
EwaGrela <kwiaciarnia_maciejka@wp.pl>
2018-03-11 22:56:05 +0100

    better version

--- app.py
+++ app.py
@@ -1,23 +1,13 @@
 from flask import Flask, render_template, request, url_for
+from user_agents import parse 
 import datetime
 
 app = Flask(__name__)
-
-
-# @app.route("/")
-# def hello():
-# 	return "Hello world!"
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
 
 @app.route("/")
 def index():
 	title ="Flask app"
 	return render_template("index.html", title=title)
-
-
-
 
 @app.route("/request")
 def request_info():
@@ -28,20 +18,19 @@ def request_info():
 def now():
 	title ="Now"
 	time = datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S.%f")
-	# datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S.%f")
 	return render_template("now.html", time=time, title=title)
 
-@app.route("/user_agent")
+@app.route("/user-agent")
 def user_agent():
 	title = "User agent"
-	return render_template("user_agent.html", title=title)
+	ua = parse(request.user_agent.string)
+	return render_template("user_agent.html", title=title, ua=ua)
 
 @app.route("/counter")
-def count():
+def counter():
 	with open("count.txt", "r") as g:
 		count = g.read()
 		count = int(count)
-		print(type(count))
 	with open("count.txt", "w") as h:
 		h.write(str(count+1))
 	return render_template("count.html", count=count)
